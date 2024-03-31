0c924208d908eb5c69ab54a211e6a96a6bcb2e14
Rita <55894059+Umitt96@users.noreply.github.com>
2024-03-31 18:44:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let wallet = 200000000;
+let wallet = 1000000;
 let price;
 let product = 1;
 let productionRate = 0;
@@ -11,7 +11,10 @@ let playerLevel = 1;
 let salary = 50;
 let progressBar1Width, progressBar2Width = 50;
 let Timerdifficulty = 1000;
-let BankLimit,BankMax;
+let caughtCount = 0;
+let sector = "kimbap";
+let BankCurrLimit = 0;
+let BankMaxLimit = 40000;
 let countdown;
 
 // DOM elemanlarını al
@@ -22,7 +25,6 @@ const productDisplay = document.getElementById("productDisplay");
 const walletDisplay = document.getElementById("wallet");
 const countdownDisplay = document.getElementById("countdown");
 const PPerSec = document.getElementById("PPerSec");
-const buyButtons = document.querySelectorAll(".buy-worker");
 const rawMadeDisplay = document.getElementById("rawMade");
 const TotalWorkers = document.getElementById("totalWorkers");
 const TaxDisplay = document.getElementById("tax");
@@ -32,10 +34,18 @@ const rawMadeBar = document.getElementById("rawMadeBar");
 const wareHouseBar = document.getElementById("wareHouseBar");
 const salePriceBar = document.getElementById("salePriceBar");
 const customRange = document.getElementById("customRange");
-const AssetButtons = document.querySelectorAll('.AssetButton');
 const modalTax = document.getElementById("modalTax");
 const modalBank = document.getElementById("modalBank");
 const modalWorker = document.getElementById("modalWorker");
+const taxCaught = document.getElementById("taxCaught");
+const Credit = document.getElementById("CurCredit");
+const Credit2 = document.getElementById("CurCredit2");
+const MaxCredit = document.getElementById("MaxCredit");
+
+const buyButtons = document.querySelectorAll(".buy-worker");
+const AssetButtons = document.querySelectorAll('.AssetButton');
+const updatedValueSpans = document.querySelectorAll('.itemName');
+
 
 function startGame() {
     const companyName = document.getElementById('companyInput').value;
@@ -59,32 +69,66 @@ function startGame() {
 const dropdownInput = document.getElementById('sectorSelect');
 
 dropdownInput.addEventListener('change', function() {
-  const selectedValue = dropdownInput.value;
-
-  const updatedValueSpans = document.querySelectorAll('.itemName');
-  
-  updatedValueSpans.forEach(function(span) {
-    span.textContent = selectedValue;
-  });
 });
 
   const updatedValueSpans = document.querySelectorAll('.itemName');
+
+
+  var sectorPhotoDiv = document.getElementById("sectorPhoto1");
+  var sectorPhotoDiv2 = document.getElementById("sectorPhoto2");
+  var sectorPhotoDiv3 = document.getElementById("sectorPhoto3");
+  
+  // Tüm sectorPhotoğrafları gizle
+  var sectorPhotos = sectorPhotoDiv.getElementsByTagName("img");
+  for (var i = 0; i < sectorPhotos.length; i++) {
+      sectorPhotos[i].style.display = "none";
+  }
+  
+  var sectorPhotos2 = sectorPhotoDiv2.getElementsByTagName("img");
+  for (var i = 0; i < sectorPhotos2.length; i++) {
+      sectorPhotos2[i].style.display = "none";
+  }
+  
+  var sectorPhotos3 = sectorPhotoDiv3.getElementsByTagName("img");
+  for (var i = 0; i < sectorPhotos3.length; i++) {
+      sectorPhotos3[i].style.display = "none";
+  }
+  
+  // Seçime göre doğru sectorPhotoğrafı göster
+  var selectedSectorPhoto = document.getElementById(dropdownInput.value + "Foto");
+  var selectedSectorPhoto2 = document.getElementById(dropdownInput.value + "Foto2");
+  var selectedSectorPhoto3 = document.getElementById(dropdownInput.value + "Foto3");
+  
+  if (selectedSectorPhoto) {
+      selectedSectorPhoto.style.display = "block";
+  }
+  
+  if (selectedSectorPhoto2) {
+      selectedSectorPhoto2.style.display = "block";
+  }
+  
+  if (selectedSectorPhoto3) {
+      selectedSectorPhoto3.style.display = "block";
+  }
+  
+
   updatedValueSpans.forEach(function(span) {
     span.textContent = dropdownInput.value;
   });
+
+ 
   
 if(settings.difficulty === "normal"){
   Timerdifficulty = 1000;
 }else if(settings.difficulty === "hard"){
   Timerdifficulty = 300;
 }
-// Kaydedilen ayarları konsola yazdır
-console.log("Zorluk Seviyesi: " + Timerdifficulty);
   
 // Her saniyede ürün üretme
 const productionInterval = setInterval(produce, Timerdifficulty);
 
   document.getElementById("startBTN").click();
+  TakeCredit()
 
 }
 
@@ -92,33 +136,39 @@ const productionInterval = setInterval(produce, Timerdifficulty);
 function updateDisplays() {
   sale_price.innerText = price + " ₺";
   sale_price2.innerText = price + " ₺";
-  walletDisplay.innerText = addCommas(wallet);
+  walletDisplay.innerText = addCommasNeg(wallet);
   productDisplay.innerText = addCommas(product);
   rawMadeDisplay.innerText = addCommas(rawMade);
   TotalWorkers.innerText = addCommas(CurrWorker);
   PPerSec.innerText = addCommas(productionRate);
   LevelDisplay.innerText = playerLevel;
-}
-updateDisplays()
-updateTax();
 
-function produce() {
-  if (rawMade <= 50) {
-    console.log("Ham madde tükendi, üretim durduruldu.");
-    
-  } else if(product >= wareHouse) {
-    console.log("Depo doldu");
-  }else{
-
-    // Ham madde miktarı negatif olmasın, minimum 0 olarak ayarla
-    rawMade = Math.max(rawMade - 5 * productionRate, 0);
-
-    product += productionRate;
-    updateRawMadeBar(rawMade-product,rawMade);
-    updateWareHouseBar(product,wareHouse);
-    updateDisplays();
+  if (wallet < 0) {
+    walletDisplay.classList.add('neg');
+  } else {
+    walletDisplay.classList.remove('neg');
   }
 }
+updateBank();
+updateTax();
+updateDisplays()
+
+function produce() {
+  if (Timerdifficulty !== 0 && rawMade > 50 && product < wareHouse) {
+    rawMade = Math.max(rawMade - 5 * productionRate, 0);
+    product += productionRate;
+    updateRawMadeBar(rawMade - product, rawMade);
+    updateWareHouseBar(product, wareHouse);
+    updateDisplays();
+  } else {
+    if (Timerdifficulty !== 0 && rawMade <= 50) {
+      toast("Ham madde tükendi, üretim durduruldu.", "#CC3337");
+    } else if (Timerdifficulty !== 0 && product >= wareHouse) {
+      toast("Depo doldu!", "#CC3337");
+    }
+  }
+}
+
 
 function updateTax(){
   if(tax == 0){
@@ -131,10 +181,26 @@ function updateTax(){
   }
 }
 
+
+function updateBank(){
+  if(tax == 0){
+    Credit.innerText = "Güncel Borç bulunmamaktadır!";
+    Credit2.innerText = "0 ₺";
+  }
+  else{
+    Credit.innerText = BankCurrLimit + " ₺";
+    Credit2.innerText = BankCurrLimit + " ₺";
+  }
+}
+
 // Satışı gerçekleştir
 function Sell(){
-  wallet += product * price;
-  console.log("Ürün satışından " + product * price +"₺ kazanıldı")
+
+  if(product == 0){
+    //null
+  }else{
+    wallet += product * price;
+  toast("Ürün satışından " + product * price +" ₺ kazanıldı", "#37CC33")
   
   switch(Timerdifficulty){
     case 1000:
@@ -145,17 +211,20 @@ function Sell(){
   product = 0;
   updateDisplays();
   updateTax();
+  }
   
 }
 
 /* Ham madde alımı */
 function buyRawAll() {
   rawMade += wallet;
+  toast("Ham madde alındı: "+ wallet + " ₺","#37CC33")
   wallet = 0;
   updateDisplays();
 }
 function buyRawHalf() {
   rawMade += parseInt(wallet / 2);
+  toast("Ham madde alındı: "+ wallet / 2 + " ₺","#37CC33")
   wallet = parseInt(wallet / 2); // Bölme işlemi sonucunu kayan noktalı sayıya dönüştür
   updateDisplays();
 }
@@ -164,13 +233,13 @@ function buyRawValue() {
   let inputQuantity = parseInt(document.querySelector('#quantityInput').value);
 
   if (inputQuantity === "" || isNaN(inputQuantity) || inputQuantity <= 0) {
-    console.log("Geçersiz miktar!");
+    toast("Geçersiz miktar!", "#CC3337");
   } else if (inputQuantity > wallet) {
-    console.log("Bakiye yetersiz!");
+    toast("Bakiye yetersiz!", "#CC3337");
   } else {
     rawMade += inputQuantity;
     wallet -= inputQuantity;
-    console.log("Ham madde satın alındı: " + inputQuantity);
+    toast("Ham madde satın alındı: " + inputQuantity,"#37CC33");
   }
 }
 
@@ -181,26 +250,59 @@ function PayTaxAll(){
   tax = 0;
   updateDisplays();
   updateTax();
-  console.log("Bütün verginiz ödenmiştir!");
+  toast("Bütün verginiz ödenmiştir!","#37CC33");
 }
 
 function PayTaxValue(){
   let inputQuantityTax = parseInt(document.querySelector('#quantityInputTax').value);
 
   if (inputQuantityTax === "" || isNaN(inputQuantityTax) || inputQuantityTax <= 0) {
-    console.log("Geçersiz miktar!");
+    toast("Geçersiz miktar!","#CC3337");
   } else if (inputQuantityTax > wallet) {
-    console.log("Bakiye yetersiz!");
+    toast("Bakiye yetersiz!","#CC3337");
   } else {
     tax -= inputQuantityTax;
     wallet -= inputQuantityTax;
     updateTax();
-    console.log("Kalan verginiz: " + tax);
+    toast("Kalan verginiz: " + tax);
   }
 }
 
 function TaxEvasion(){
-  console.log("Vergi kaçırıldı, afiyet bal şeker!");
+  if(playerLevel >= 2 && tax > 10000){
+  if (Math.random() <= 0.6) {
+    tax = 0;
+    updateDisplays();
+    updateTax();
+    toast("Vergi kaçırıldı, afiyet bal şeker!","#37CC33");
+} else {
+  caughtCount++; 
+  taxCaught.innerText = caughtCount;
+  if (caughtCount === 1) {
+    toast("Yakalandın! Kaçırdığın miktarın 2 katı borcun var artık!","#CC3337");
+    wallet -= tax*2;
+    updateTax();
+
+  } else if (caughtCount === 2) {
+    toast("İkinci kez yakalandın. Daha dikkatli ol!","#CC3337");
+    wallet -= tax*5;
+    updateTax();
+    playerLevel-=1;
+
+  } else {
+    toast("Vergi kaçırdığın için hapse girdin ve işletmen kapatıldı!","#CC3337");
+    toast("Oyun bitti...","#CC3337");
+
+    timer = 0;
+    wallet = 0;
+    Timerdifficulty = 0;
+    rawMade = 51;
+  }
+}
+}else{
+  toast("Bu kadar az vergiyi kaçıramazsın :/");
+}
+
 }
 
 // Satış fiyatını güncelleme fonksiyonu
@@ -226,11 +328,11 @@ function updateRawMadeBar(rawMadeValue, maxValue) {
     const workPrice = parseInt(button.getAttribute("data-price"));
 
     if (wallet >= workPrice) {
-      button.classList.add("greenButton"); // Yeşil stili ekle
-      button.classList.remove("redButton"); // Kırmızı stili kaldır
+      button.classList.add("greenButton"); 
+      button.classList.remove("redButton"); 
     } else {
-      button.classList.add("redButton"); // Kırmızı stili ekle
-      button.classList.remove("greenButton"); // Yeşil stili kaldır
+      button.classList.add("redButton"); 
+      button.classList.remove("greenButton"); 
     }
   });
 }
@@ -241,32 +343,35 @@ function buyWorker(workRate, workPrice) {
   CurrWorker += 1;
   wallet -= workPrice;
   updateDisplays();
-  console.log("Üretim hızı arttırıldı:", productionRate);
-  updateButtonColors(); // Buton renklerini güncelle
+  updateButtonColors(); 
+  toast("Yeni personel alındı! Güncel çalışan sayısı: "+ CurrWorker, "#37CC33")
 }
 
-
 function saveSalary() {
-
+  if(playerLevel >= 2)
+  {
   value = parseInt(customRange.value);
   salary =     value;
 
-  console.log("Çalışan memnuniyeti: %" + salary); 
-  console.log("maaş kaydedildi!");
+  toast("Çalışan memnuniyeti: %" + salary); 
 
   setTimeout(function() {
     if(salary <= 30){
-      console.log("Çalışanlar bütün ürünleri yakarak isyan çıkardı! Yarra Yering alkolünden ister misin?");
+      toast("Çalışanlar bütün ürünleri yakarak isyan çıkardı! Yarra Yering şarabından ister misin?","#CC3337");
       Decrease_work = parseInt(CurrWorker/3);
 
       productionRate -= Decrease_work;
       CurrWorker -= Decrease_work;
-      console.log(Decrease_work +" işçi kaybetttin!");
+      toast(Decrease_work +" işçi kaybetttin!","#CC3337");
       updateDisplays();
 
 
     }
   }, (Math.floor(Math.random() * 5000) + 5000)); 
+  }
+  else{
+    toast("Bunu yapmak için 2 seviye ve üstü olman gerekir!","#CC3337")
+  }
 }
 
 customRange.addEventListener("input", updateProgressBars);
@@ -289,14 +394,11 @@ function updateProgressBars() {
   progressBar2.style.width = progressBar2Width; 
 }
 
-
-
-
 function buyWareHouse(wareRate, warePrice) {
   wareHouse += wareRate;
   wallet -= warePrice;
   updateDisplays();
-  console.log("Depo kapasitesi arttırıldı:", wareHouse);
+  toast("Depo kapasitesi arttırıldı: "+ wareHouse, "#37CC33");
   updateButtonColors(); // Buton renklerini güncelle
 }
 
@@ -305,29 +407,41 @@ function buyAsset(AssetPrice,levelValue) {
   playerLevel += 1;
   wallet -= AssetPrice;
   updateDisplays();
-  console.log(playerLevel+ " Seviye oldunuz!");
+  toast(playerLevel+ " Seviye oldunuz!", "#37CC33");
   updateButtonColors();
-  
+  let randomTime = Math.floor(Math.random() * 5000) + 30000; 
+
   switch (levelValue) {
     case 2:
       modalTax.innerText =  "80.000 ₺";
       modalBank.innerText = "40.000 ₺";
       modalWorker.innerText = "Usta işçi";
+
+      setTimeout(() => {
+        toast("Bir yüksek getirisi olan bir şirketi satın aldın!", "#37CC33")
+        wallet += 50000;
+        toast("Kazanılan miktar: 50.000 ₺", "#37CC33")
+        updateDisplays();
+    }, randomTime);
       break;
     case 3:
       modalTax.innerText =  "150.000 ₺";
       modalBank.innerText = "100.000 ₺";
       modalWorker.innerText = "Yönetici";
+
+    setTimeout(() => {
+        toast("Eve giderken kaza yaptın :(","#CC3337")
+        wallet -= 200000;
+        toast("Hastane masrafları: 20.000 ₺","#CC3337")
+        updateDisplays();
+    }, randomTime);
+
+
       break;
       case 4:
         modalTax.innerText =  "400.000 ₺";
         modalBank.innerText = "500.000 ₺";
         modalWorker.innerText = "Uzman yönetici";
-        break;
-      case 5:
-        modalTax.innerText =  "--- ₺";
-        modalBank.innerText = "--- ₺";
-        modalWorker.innerText = "---";
         break;
   }
 }
@@ -339,6 +453,12 @@ function updatePrice() {
   sale_price2.textContent = price + " ₺";
   sale_price.textContent = price + " ₺";
   updateSalePriceBar(price, 20);
+  toast("Piyasa fiyatı güncellendi! Yeni fiyat: "+ price + " ₺")
+
+  if(BankCurrLimit != 0){
+    BankCurrLimit  += BankCurrLimit* 0.1;
+    updateBank();
+  }
 }
 
 // Rastgele fiyat üretme 
@@ -348,6 +468,41 @@ function getRandomNumber() {
     return Math.floor(Math.random() * 10) + 4; // 4 ile 13 arasında rastgele bir sayı
   } else {
     return Math.floor(Math.random() * 6) + 20; // 20 ile 25 arasında rastgele bir sayı
+  }
+  
+}
+
+// Banka işlemleri 
+function TakeCredit(){
+  
+  let inputCredit = parseInt(document.querySelector('#inputCredit').value);
+  MaxCredit.innerText = BankMaxLimit+ " ₺";
+  
+  if (inputCredit === "" || isNaN(inputCredit) || inputCredit <= 0) {
+    toast("Geçersiz miktar!", "#CC3337");
+  } else if (inputCredit > BankMaxLimit - BankCurrLimit) {
+    toast("O kadar limitiniz yok!", "#CC3337");
+  } else {
+    wallet += inputCredit;
+    BankCurrLimit += inputCredit;
+    updateBank();
+    toast("Bankadan Kredi çekildi: " + inputCredit + " ₺", "#37CC33");
+  }
+}
+
+function PayCredit(){
+  
+  let inputCredit = parseInt(document.querySelector('#inputCredit').value);
+  
+  if (inputCredit === "" || isNaN(inputCredit) || inputCredit <= 0) {
+    toast("Geçersiz miktar!","#CC3337");
+  } else if (inputCredit > BankMaxLimit || wallet < inputCredit) {
+    toast("Limitiniz kadar ödeme yapabilirsiniz","#CC3337");
+  } else {
+    wallet -= inputCredit;
+    BankCurrLimit -= inputCredit;
+    updateBank();
+    toast("Bankaya borç ödendi: " + inputCredit + " ₺","#37CC33");
   }
 }
 
@@ -369,7 +524,6 @@ function startTimer(duration, display) {
   }, Timerdifficulty);
 }
 
-
 // Oyunu başlat
 window.onload = function () {
   var countdownDuration = 2 * 60 + 59;
@@ -378,6 +532,14 @@ window.onload = function () {
 
   updatePrice();
 };
+
+//Oyunu bitir
+function FinishGame(price){
+  playerLevel += 1;
+  wallet -= price;
+  updateDisplays();
+  toast("En yüksek seviyeye ulaştınız, Tebrikler!", "#37CC33");
+}
 
 function addCommas(number) {
   let strNumber = String(number);
@@ -390,6 +552,29 @@ function addCommas(number) {
       formattedNumber = '.' + formattedNumber;
     }
   }
+  return formattedNumber;
+}
+
+function addCommasNeg(number) {
+  let strNumber = String(number);
+  let isNegative = strNumber.startsWith('-'); // Negatif sayı kontrolü
+  
+  // Negatif işareti çıkart
+  if (isNegative) {
+    strNumber = strNumber.slice(1);
+  }
+  
+  let formattedNumber = '';
+  for (let i = strNumber.length - 1, j = 1; i >= 0; i--, j++) {
+    formattedNumber = strNumber[i] + formattedNumber;
+    if (j % 3 === 0 && i !== 0) {
+      formattedNumber = '.' + formattedNumber;
+    }
+  }
+  if (isNegative) {
+    formattedNumber = '-' + formattedNumber;
+  }
+  
   return formattedNumber;
 }
 
@@ -416,3 +601,22 @@ function openCity(evt, cityName) {
         button.disabled = true;
       });
     });
+
+
+    function toast(message,color) {
+      Toastify({
+          text: message,
+          duration: 2000,
+          newWindow: false,
+          close: true,
+          gravity: "bottom",
+          position: "right",
+          stopOnFocus: true,
+          style: {
+              background: color,
+              borderRadius: "10px",
+          },
+          onClick: function() {} 
+      }).showToast();
+  }
+  
